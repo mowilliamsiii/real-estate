c0e34362e728730aa5514d8938fbf2673f58d7ae
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-08-20 13:10:17 -0400

    SQL update -MW

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,3 @@
 insert into house
 (id,street_name, street_number, city_name, apartment_number)
     values('1','La La Lane', 123,'Gotham','A5' );
--- insert into house
--- (id,street_name, street_number, city_name, apartment_number)
--- values('2','Fa a La La Lane', 234,'Gotham','A6' );
--- insert into house
--- (id,street_name, street_number, city_name, apartment_number)
--- values('3','blah La La Lane', 567,'Gotham','A7' );
-
---     this.id = id;
--- this.streetName = streetName;
---         this.streetNumber = streetNumber;
---         this.cityName = cityName;
---         this.apartmentNumber = apartmentNumber;
